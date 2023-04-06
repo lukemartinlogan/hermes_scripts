e05a613aaa2ad0a873f40027bb4c3d3ac2b938aa
lukemartinlogan <lukemartinlogan@gmail.com>
2023-04-06 17:56:37 -0500

    Make paths more generic

--- ares/hermes_paths.sh
+++ ares/hermes_paths.sh
@@ -5,16 +5,16 @@ cd ${CMAKE_SOURCE_DIR}/build
 CMAKE_BINARY_DIR=`pwd`
 HERMES_TRAIT_PATH=${CMAKE_BINARY_DIR}/bin
 
-mkdir /mnt/nvme/llogan/test_hermes
-mkdir /mnt/ssd/llogan/test_hermes
+mkdir /mnt/nvme/${USER}/test_hermes
+mkdir /mnt/ssd/${USER}/test_hermes
 mkdir ${HOME}/test_hermes
-mkdir /mnt/ssd/llogan/ior_out
+mkdir /mnt/ssd/${USER}/ior_out
 mkdir ${HOME}/ior_out
 
 cleanup_hermes() {
-  rm -rf /mnt/nvme/llogan/test_hermes/*
-  rm -rf /mnt/ssd/llogan/test_hermes/*
-  rm -rf /mnt/ssd/llogan/ior_out/*
+  rm -rf /mnt/nvme/${USER}/test_hermes/*
+  rm -rf /mnt/ssd/${USER}/test_hermes/*
+  rm -rf /mnt/ssd/${USER}/ior_out/*
   rm -rf ${HOME}/test_hermes/*
   rm -rf ${HOME}/ior_out/*
 }
