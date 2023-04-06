b2bc13cdca71e3be95c8ddfbc472ac4ee0621e18
lukemartinlogan <lukemartinlogan@gmail.com>
2023-04-06 16:22:50 -0500

    SSD final dir

--- ares/ior_write_tiered.sh
+++ ares/ior_write_tiered.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 HERMES_SCRIPTS_ROOT=`pwd`
-. ${HERMES_SCRIPTS_ROOT}/ares/ior.sh "${HOME}/test_hermes/hi.txt"
+# . ${HERMES_SCRIPTS_ROOT}/ares/ior.sh "${HOME}/test_hermes/hi.txt"
+. ${HERMES_SCRIPTS_ROOT}/ares/ior.sh "/mnt/ssd/llogan/hi.txt"
 
 # HERMES
 echo "HERMES TESTS BEGIN"
-ior_write_posix_fpp "40" "4GB" "1m"
+# ior_write_posix_fpp "40" "4GB" "1m"
 ior_write_hermes_posix_fpp "40" "4GB" "1m" "_ssd"
 ior_write_hermes_posix_fpp "40" "4GB" "1m" "_ssd_nvme"
 ior_write_hermes_posix_fpp "40" "4GB" "1m" "_ssd_nvme_ram"
