960fc926eb5c9472691ac1c96c6a7f4a02c2b79f
lukemartinlogan <lukemartinlogan@gmail.com>
2023-04-06 16:31:56 -0500

    Make bandwidths for SSD and PFS correct. Add simple IOR test

---
+++ ares/ior_simple.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+HERMES_SCRIPTS_ROOT=`pwd`
+. ${HERMES_SCRIPTS_ROOT}/ares/ior.sh "${HOME}/test_hermes/hi.txt"
+
+# Hermes
+echo "HERMES TESTS BEGIN"
+ior_write_hermes_posix_fpp "1" "16MB" "1m" "_pfs_ssd_nvme_ram"
