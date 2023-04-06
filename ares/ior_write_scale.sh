185c124e2480b911ca40112478779411a581c1c0
lukemartinlogan <lukemartinlogan@gmail.com>
2023-04-06 14:21:31 -0500

    Use better sizes

--- ares/ior_write_scale.sh
+++ ares/ior_write_scale.sh
@@ -5,7 +5,7 @@ HERMES_SCRIPTS_ROOT=`pwd`
 
 # Hermes
 echo "HERMES TESTS BEGIN"
-ior_write_hermes_posix_fpp "10" "10GB" "1m" "_pfs_ssd_nvme_ram"
-ior_write_hermes_posix_fpp "20" "5GB" "1m" "_pfs_ssd_nvme_ram"
-ior_write_hermes_posix_fpp "30" "2.5GB" "1m" "_pfs_ssd_nvme_ram"
-ior_write_hermes_posix_fpp "40" "1.25GB" "1m" "_pfs_ssd_nvme_ram"
+ior_write_hermes_posix_fpp "10" "12GB" "1m" "_pfs_ssd_nvme_ram"
+ior_write_hermes_posix_fpp "20" "8GB" "1m" "_pfs_ssd_nvme_ram"
+ior_write_hermes_posix_fpp "30" "5GB" "1m" "_pfs_ssd_nvme_ram"
+ior_write_hermes_posix_fpp "40" "4GB" "1m" "_pfs_ssd_nvme_ram"
