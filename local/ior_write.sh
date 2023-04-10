0ac8e5f3170164ac67e3da68ca5a729f862c05ca
lukemartinlogan <lukemartinlogan@gmail.com>
2023-04-10 07:04:14 -0500

    Write only to RAM

--- local/ior_write.sh
+++ local/ior_write.sh
@@ -4,6 +4,4 @@ HERMES_SCRIPTS_ROOT=`pwd`
 . ${HERMES_SCRIPTS_ROOT}/local/ior.sh "release"
 
 # IOR write with posix
-ior_write_hermes_posix_fpp "1" "64GB" "1m" "_ram"
-ior_write_hermes_posix_fpp "2" "32GB" "1m" "_ram"
-ior_write_hermes_posix_fpp "4" "16GB" "1m" "_ram"
+ior_write_hermes_posix_fpp "4" "4GB" "1m" "_ram"
