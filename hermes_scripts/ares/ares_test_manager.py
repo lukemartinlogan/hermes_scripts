91e2c2d7e6916356b256a1e5cbc4cd14a48e62d1
lukemartinlogan <lukemartinlogan@gmail.com>
2023-04-12 12:18:01 -0500

    32GB per node

--- hermes_scripts/ares/ares_test_manager.py
+++ hermes_scripts/ares/ares_test_manager.py
@@ -155,20 +155,22 @@ class AresTestManager(TestManager):
     """ IOR Tests (NO HERMES) """
     """======================================================================"""
     def test_ram_bw(self):
-        self.memcpy_test_cmd(1, '1m', 32768 / 2)
-        self.memcpy_test_cmd(2, '1m', 16384 / 2)
-        self.memcpy_test_cmd(4, '1m', 8192 / 2)
-        self.memcpy_test_cmd(8, '1m', 4096 / 2)
-        self.memcpy_test_cmd(16, '1m', 2048 / 2)
-        self.memcpy_test_cmd(32, '1m', 1024 / 2)
+        count = SizeConv.to_int('16g') / SizeConv.to_int('1m')
+        self.memcpy_test_cmd(1, '1m', count)
+        self.memcpy_test_cmd(2, '1m', count / 2)
+        self.memcpy_test_cmd(4, '1m', count / 4)
+        self.memcpy_test_cmd(8, '1m', count / 8)
+        self.memcpy_test_cmd(16, '1m', count / 16)
+        self.memcpy_test_cmd(32, '1m', count / 32)
 
     def test_ram_latency(self):
-        self.memcpy_test_cmd(1, '4k', 32768 / 2)
-        self.memcpy_test_cmd(2, '4k', 16384 / 2)
-        self.memcpy_test_cmd(4, '4k', 8192 / 2)
-        self.memcpy_test_cmd(8, '4k', 4096 / 2)
-        self.memcpy_test_cmd(16, '4k', 2048 / 2)
-        self.memcpy_test_cmd(32, '4k', 1024 / 2)
+        count = SizeConv.to_int('16g') / 4096
+        self.memcpy_test_cmd(1, '4k', count)
+        self.memcpy_test_cmd(2, '4k', count / 2)
+        self.memcpy_test_cmd(4, '4k', count / 4)
+        self.memcpy_test_cmd(8, '4k', count / 8)
+        self.memcpy_test_cmd(16, '4k', count / 16)
+        self.memcpy_test_cmd(32, '4k', count / 32)
 
     def test_ior_backends(self):
         self.ior_write_cmd(1, '1m', '1g', backend='posix')
@@ -187,17 +189,17 @@ class AresTestManager(TestManager):
     """ IOR Tests (HERMES) """
     """======================================================================"""
     def test_hermes_ior_write_tiered(self):
-        self.ior_write_cmd(4, '1m', '4g',
+        self.ior_write_cmd(8, '1m', '4g',
                            hermes_mode='kScratch',
                            hermes_conf='hermes_server_ssd.yaml',
                            backend='posix',
                            dev='ssd')
-        self.ior_write_cmd(4, '1m', '4g',
+        self.ior_write_cmd(8, '1m', '4g',
                            hermes_mode='kScratch',
                            hermes_conf='hermes_server_ssd_nvme.yaml',
                            backend='posix',
                            dev='ssd')
-        self.ior_write_cmd(4, '1m', '4g',
+        self.ior_write_cmd(8, '1m', '4g',
                            hermes_mode='kScratch',
                            hermes_conf='hermes_server_ssd_nvme_ram.yaml',
                            backend='posix',
