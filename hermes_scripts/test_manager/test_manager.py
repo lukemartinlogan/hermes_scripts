0db705df37f81cda2702259f0af119eb0a21d061
lukemartinlogan <lukemartinlogan@gmail.com>
2023-04-17 05:05:09 -0500

    Cleanup with PSSH

--- hermes_scripts/test_manager/test_manager.py
+++ hermes_scripts/test_manager/test_manager.py
@@ -185,7 +185,7 @@ class TestManager(ABC):
             files = os.listdir(dir)
             for file in files:
                 Rm(os.path.join(dir, file, '*'),
-                   PsshExecInfo(hostfile=self.spawn_all_nodes()))
+                   PsshExecInfo(hostfile=self.spawn_all_nodes().hostfile))
 
     def find_tests(self):
         # Filter the list to include only attributes that start with "test"
