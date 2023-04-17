8f821b6fcf9bb7cb8513ca1a88d041612fd16491
lukemartinlogan <lukemartinlogan@gmail.com>
2023-04-17 05:14:22 -0500

    Increase time to 20s

--- hermes_scripts/test_manager/test_manager.py
+++ hermes_scripts/test_manager/test_manager.py
@@ -240,7 +240,7 @@ class TestManager(ABC):
                                collect_output=True,
                                hide_output=False,
                                exec_async=True))
-        time.sleep(12)
+        time.sleep(20)
         print("Launched")
 
     def stop_daemon(self, spawn_info):
