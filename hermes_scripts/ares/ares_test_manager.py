ea3843d7fd43a3eda5ee52d386aa4a0e6968b98d
lukemartinlogan <lukemartinlogan@gmail.com>
2023-06-02 07:01:40 -0500

    MN IOR Write

--- hermes_scripts/ares/ares_test_manager.py
+++ hermes_scripts/ares/ares_test_manager.py
@@ -390,7 +390,7 @@ class AresTestManager(TestManager):
         self.test_ram_bw()
 
     def test_ior_write(self):
-        num_nodes_set = [1]
+        num_nodes_set = [4]
         ppn_set = [16]
         test_cases = itertools.product(num_nodes_set, ppn_set)
         for num_nodes, ppn in test_cases:
